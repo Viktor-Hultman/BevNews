6b7d54c844b26ade8ed3adfa4ed3a2b0ce5ceefc
justussundelof <justus.sundelof@student.kyh.se>
2021-03-09 09:46:58 +0100

    added sign in button

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+import * as ROUTES from '../../constants/routes';
+
+const SignInButton = styled.button`
+width: 100px;
+border-radius: 10px;
+ltext-decoration: none;
+`
+
 const Landing = () => (
     <div>
         <h1>Landing</h1>
+        <p></p>
+        <SignInButton><Link to={ROUTES.SIGN_IN}>Sign In</Link></SignInButton>
     </div>
 );
 
 export default Landing;
+
