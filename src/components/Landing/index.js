41f06dfc44223d62e2a684f943c1d6bc51b67e93
Viktor Hultman <viktor-hultman@hotmail.com>
2021-03-10 10:52:29 +0100

    Added a local json file containing instagram data about the keyword 'tesla' and use the data to display some info also added some more information to the landing page, removed message functionality from home and added a global style to the app.

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,16 +1,22 @@
-
+import ExampleGraph from '../Graphs/ExampleGraph'
 
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 import * as ROUTES from '../../constants/routes';
 
+
 const SignInButton = styled.button`
 width: 80px;
-height: 28px;
-border-radius: 10px;
+height: 35px;
+border-radius: 8px;
 background: #C4C4C4;
+margin: 20px;
 `
+
+// const BiggerSignInButton = styled(SignInButton)`
+// height: 80px;
+// `
 
 const ButtonLink = styled(Link)`
 text-decoration: none;
@@ -22,24 +28,41 @@ display: flex;
 flex-direction: column;
 align-items: center;
 width: 100vw;
-height: 100%;
+height: 100vh;
+text-align: center;
 
+// By using the ">" symbol, only the direct children instead
+// of also targeting grandchildren
+& > h1 {
+    padding: 20px 10px 20px 10px;
+}
+
+& > p {
+    padding: 20px;
+}
 `
 
 const ExampleGraphDiv = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+justify-content: center;
 width: 100%;
-height: 100vh;
+height: 100%;
 background: #CCCCCC;
-margin-top: 50px;
 
 `
 
 const Landing = () => (
     <LandingDiv>
         <h1>Landing</h1>
-        <p></p>
-        <SignInButton><ButtonLink to={ROUTES.SIGN_IN}>Sign In</ButtonLink></SignInButton>
+        <p>
+            *Here will some general information about the app be displayed,
+             maybe what the consumer can use the app for. As well as describe some features.*  
+        </p>
+        <SignInButton><p><ButtonLink to={ROUTES.SIGN_IN}>Sign In</ButtonLink></p></SignInButton>
         <ExampleGraphDiv>
+            <ExampleGraph />
         </ExampleGraphDiv>
     </LandingDiv>
 );
--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components';
 import App from './components/App';
 
 import Firebase, { FirebaseContext } from './components/Firebase';
 
+
+const GlobalStyle = createGlobalStyle`
+* {
+  margin: 0;
+  padding: 0;
+  
+}
+
+::-webkit-scrollbar {
+  display: none;
+}
+`
+
+
 ReactDOM.render(
   <React.StrictMode>
     <FirebaseContext.Provider value={new Firebase()}>
+      <GlobalStyle />
       <App />
     </FirebaseContext.Provider>
   </React.StrictMode >,
