0b0de083c19af1ab0c85e04ab1fe922c6a73e20d
Viktor Hultman <viktor-hultman@hotmail.com>
2021-03-15 11:31:20 +0100

    Added search function with a keyword and date inputs on the home page.

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 import { AuthUserContext, withAuthorization } from '../Session';
 import { withFirebase } from '../Firebase';
 
+import HomeSearch from '../FetchSearch';
+
 const HomePage = () => (
     <div>
         <h1>Home Page</h1>
         <p>The Home Page is accessible by every signed in user.</p>
-
+        <HomeSearch />
 
     </div>
 );
