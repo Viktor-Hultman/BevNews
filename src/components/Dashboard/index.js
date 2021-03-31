f488f81ca3fcb7cc080b744893898fb27658bd63
Viktor Hultman <viktor-hultman@hotmail.com>
2021-03-31 13:51:32 +0200

    Remade the functions for getting our timestamps by using arrays and a for loop

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -5,10 +5,10 @@ import { withFirebase } from '../Firebase';
 import { AuthUserContext } from '../Session';
 
 const Dashboard = ({ firebase }) => {
- 
-    const [userWord1, setUserWord1] = useState("")   
-    const [userWord2, setUserWord2] = useState("")    
-    const [userWord3, setUserWord3] = useState("")   
+    //Several useStates for setting different states we will use later
+    const [userWord1, setUserWord1] = useState("")
+    const [userWord2, setUserWord2] = useState("")
+    const [userWord3, setUserWord3] = useState("")
 
     const [userCountry, setUserCountry] = useState("")
 
@@ -16,9 +16,10 @@ const Dashboard = ({ firebase }) => {
 
     const [formattedTodayDate, setFormattedTodayDate] = useState("")
     const [formatted1WeekAgo, setFormatted1WeekAgo] = useState("")
-    const [formatted2WeekAgo, setFormatted2WeekAgo] = useState ("")
-    const [formatted3WeekAgo, setFormatted3WeekAgo] = useState ("")
+    const [formatted2WeekAgo, setFormatted2WeekAgo] = useState("")
+    const [formatted3WeekAgo, setFormatted3WeekAgo] = useState("")
 
+    //Setting some variables that we need for creating the full url we'll use in our api fetches
     const Url = "https://gnews.io/api/v4/search?q="
     const Key = "&token=8bd8954322ec49530ab22e22c8a3b84f"
     const Country = "&country"
@@ -27,144 +28,139 @@ const Dashboard = ({ firebase }) => {
     const Time = "T00:00:01Z"
     const To = "&to="
 
-    //Here we get the full URL from the user, it contains one search word, the selected country and language
+    //Here we get the full URL from the user, it contains one search word, a from date, a to date, the selected country and language
     console.log(Url + userWord1 + From + formatted1WeekAgo + Time + To + formattedTodayDate + Time + Country + userCountry + Lang + userLanguage + Key)
 
+    //Getting the unique id of the signed in user from the context provider
+    //so we can use it to link the user to their firebase data profile
     let { uid } = useContext(AuthUserContext);
 
-    const timestamp = Date.now() 
-    const timeDate = new Date(timestamp)
-    console.log(timeDate)
-    console.log(timestamp)
-    const getWords = () => {
-        console.log(userWord1);
-    }
+    //Here we gather the timestap of when the page loads 
+    const timestamp = Date.now()
+    //The other timestamps get greated using 1 week as milliseconds
+    let oneWeekAgoTimestamp = timestamp - 604800000
+    let twoWeeksAgoTimestamp = timestamp - 1209600000
+    let threeWeeksAgoTimestamp = timestamp - 1814400000
 
-    let today = new Date(timestamp),
-    todayDate = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    console.log(todayDate)
+    //Creates variable to use later
+    let todayDate
+    let oneWeekAgoDate
+    let twoWeeksAgoDate
+    let threeWeeksAgoDate
 
-    let oneWeekAgoTimestamp = timestamp-604800000
-    let oneWeekAgo = new Date(oneWeekAgoTimestamp),
-    oneWeekAgoDate = oneWeekAgo.getFullYear() + '-' + (oneWeekAgo.getMonth() + 1) + '-' + oneWeekAgo.getDate();
-    console.log(oneWeekAgoDate)
 
-    let twoWeeksAgoTimestamp = timestamp-1209600000
-    let twoWeeksAgo = new Date(twoWeeksAgoTimestamp),
-    twoWeeksAgoDate = twoWeeksAgo.getFullYear() + '-' + (twoWeeksAgo.getMonth() + 1) + '-' + twoWeeksAgo.getDate();
-
-    let threeWeeksAgoTimestamp = timestamp-1814400000
-    let threeWeeksAgo = new Date(threeWeeksAgoTimestamp),
-    threeWeeksAgoDate = threeWeeksAgo.getFullYear() + '-' + (threeWeeksAgo.getMonth() + 1) + '-' + threeWeeksAgo.getDate();
-    
-
-    let zero = 0
-
-    function addStr(str, index, stringToAdd){
+    //A function that takes a string, the index of where to place the new string, and the new string to add
+    function addStr(str, index, stringToAdd) {
         return str.substring(0, index) + stringToAdd + str.substring(index, str.length);
-      }
+    }
 
     useEffect(() => {
-        if (todayDate.charAt(5) === 1){
-            console.log("this date is correct")
-        } else { 
-            setFormattedTodayDate(addStr(todayDate, 5, zero))
-        }
+        //Places the variable in corresponding order in arrays to cycle through in the for loop below
+        let weekDatesArr = [todayDate, oneWeekAgoDate, twoWeeksAgoDate, threeWeeksAgoDate]
+        let timestampArr = [timestamp, oneWeekAgoTimestamp, twoWeeksAgoTimestamp, threeWeeksAgoTimestamp]
 
-        if (oneWeekAgoDate.charAt(5) === 1){
-            console.log("this date is correct")
-        } else { 
-            setFormatted1WeekAgo(addStr(oneWeekAgoDate, 5, zero))
+        //For loop that takes the timestamps and recreates them as dates we can use in our fetch
+        for (let i = 0; i < timestampArr.length; i++) {
+            weekDatesArr[i] = new Date(timestampArr[i])
+            weekDatesArr[i] = weekDatesArr[i].getFullYear() + '-' + (weekDatesArr[i].getMonth() + 1) + '-' + weekDatesArr[i].getDate();
+            
+            if (weekDatesArr[i].charAt(5) === 1) {
+                console.log(weekDatesArr[i] + ": " + "this date is correct")
+            } else {
+                weekDatesArr[i] = (addStr(weekDatesArr[i], 5, 0))
+            }
+            console.log(weekDatesArr[i])
         }
+        //Sets the useStates of the dates that we'll use for fetching 
+        setFormattedTodayDate(weekDatesArr[0])
+        setFormatted1WeekAgo(weekDatesArr[1])
+        setFormatted2WeekAgo(weekDatesArr[2])
+        setFormatted3WeekAgo(weekDatesArr[3])
+    }, []);
 
-        if (twoWeeksAgoDate.charAt(5) === 1){
-            console.log("this date is correct")
-        } else { 
-            setFormatted2WeekAgo(addStr(twoWeeksAgoDate, 5, zero))
-        }
-        if (threeWeeksAgoDate.charAt(5) === 1){
-            console.log("this date is correct")
-        } else { 
-            setFormatted3WeekAgo(addStr(threeWeeksAgoDate, 5, zero))
-        }
-       
-    },[]);
-    console.log(formatted3WeekAgo)
-    console.log(formatted2WeekAgo)
-    console.log(formattedTodayDate)
-    console.log(userLanguage)
-    console.log(userCountry)
-    console.log(userWord1, userWord2, userWord3)
 
-    useEffect(() => { 
+   
+    // //Big chunk of logging to the console for checking values
+    // console.log(formatted3WeekAgo)
+    // console.log(formatted2WeekAgo)
+    // console.log(formatted1WeekAgo)
+    // console.log(formattedTodayDate)
+    // console.log(userLanguage)
+    // console.log(userCountry)
+    // console.log(userWord1, userWord2, userWord3)
+
+    useEffect(() => {
         const unsubscribe = firebase.user(uid).child('settings').child('searchWords')
-        .on('value', snapshot => { 
-            if (snapshot) {
-                const searchWordsObject = snapshot.val();
-                if (searchWordsObject) {
-                    let searchWordArray = Object.keys(searchWordsObject)
-                    setUserWord1(searchWordArray[0])
-                    setUserWord2(searchWordArray[1])
-                    setUserWord3(searchWordArray[2])
+            .on('value', snapshot => {
+                if (snapshot) {
+                    const searchWordsObject = snapshot.val();
+                    if (searchWordsObject) {
+                        let searchWordArray = Object.keys(searchWordsObject)
+                        //Here we take our array of the users searchwords from firebase and
+                        //"set" in their own useState variable
+                        setUserWord1(searchWordArray[0])
+                        setUserWord2(searchWordArray[1])
+                        setUserWord3(searchWordArray[2])
 
-                } else {
-                    setUserWord1("");
-                    setUserWord2("");
-                    setUserWord3("");
+                    } else {
+                        //Resets the useState variables if something happens to the users words
+                        setUserWord1("");
+                        setUserWord2("");
+                        setUserWord3("");
+                    }
                 }
-            }
-        });
-    return () => {
-        unsubscribe();
-    }
-
-}, []);
-
-useEffect(() => { 
-    const unsubscribe = firebase.user(uid).child('settings').child('country')
-    .on('value', snapshot => {
-        if (snapshot) {
-            const countryObj = snapshot.val();
-            if (countryObj) {
-                let userCountry = Object.keys(countryObj)
-                setUserCountry(userCountry[0])
-
-            } else {
-               setUserCountry("")
-            }
+            });
+        return () => {
+            unsubscribe();
         }
-    });
-return () => {
-    unsubscribe();
-}
 
-}, []);
+    }, []);
 
-useEffect(() => { 
-    const unsubscribe = firebase.user(uid).child('settings').child('language')
-    .on('value', snapshot => {
-        if (snapshot) {
-            const languageObject = snapshot.val();
-            if (languageObject) {
-                let userLanguageArr = Object.keys(languageObject)
-                setUserLanguage(userLanguageArr[0])
+    //In the two useEffects below we do the same with the users selected country and language
+    useEffect(() => {
+        const unsubscribe = firebase.user(uid).child('settings').child('country')
+            .on('value', snapshot => {
+                if (snapshot) {
+                    const countryObj = snapshot.val();
+                    if (countryObj) {
+                        let userCountry = Object.keys(countryObj)
+                        setUserCountry(userCountry[0])
 
-            } else {
-               setUserLanguage("")
-            }
+                    } else {
+                        setUserCountry("")
+                    }
+                }
+            });
+        return () => {
+            unsubscribe();
         }
-    });
-return () => {
-    unsubscribe();
-}
 
-}, []);
+    }, []);
+
+    useEffect(() => {
+        const unsubscribe = firebase.user(uid).child('settings').child('language')
+            .on('value', snapshot => {
+                if (snapshot) {
+                    const languageObject = snapshot.val();
+                    if (languageObject) {
+                        let userLanguageArr = Object.keys(languageObject)
+                        setUserLanguage(userLanguageArr[0])
+
+                    } else {
+                        setUserLanguage("")
+                    }
+                }
+            });
+        return () => {
+            unsubscribe();
+        }
+
+    }, []);
 
 
     return (
         <>
             <h1>Dashboard</h1>
-            <button onClick={getWords}>Get words</button>
         </>
     )
 
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,4 +1,4 @@
-import ExampleGraph from '../Graphs/ExampleGraph'
+
 
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
